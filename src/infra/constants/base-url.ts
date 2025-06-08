b16e47fd226585a03ce84c2c776f43f0571eed5d
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-06-08 17:59:44 -0300

    fix: change url prod

--- src/infra/constants/base-url.ts
+++ src/infra/constants/base-url.ts
@@ -2,5 +2,5 @@ const URL = process.env.NEXT_PUBLIC_BASE_URL
 
 export const baseUrl = 
   process.env.NODE_ENV === 'production'
-    ? URL!
+    ? `${URL}api/property/`!
     : 'http://localhost:3000/api/property/';
